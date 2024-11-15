564007a18311deb2478e7a52ab0942fa785613fa
Ashkree <maveron.tyriel@gmail.com>
2024-11-15 07:15:23 +0400

    remoed additional axios impot

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-import axios from "axios";
-
 const axiosInstance = axios.create({
 	baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000", // Fallback for local dev
 	headers: {
